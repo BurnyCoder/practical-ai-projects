4818648045920db67a3669c3580ec50019692201
BurnyCoder <happymancz@email.cz>
2024-08-19 22:49:31 +0200

    added fastai excercises

---
+++ image_classifier_resnet34.py
@@ -0,0 +1,18 @@
+from fastai.vision.all import *
+
+path = untar_data(URLs.PETS)/'images'
+
+def is_cat(x): return x[0].isupper()
+dls = ImageDataLoaders.from_name_func(
+    path, get_image_files(path), valid_pct=0.2, seed=42,
+    label_func=is_cat, item_tfms=Resize(224))
+
+learn = vision_learner(dls, resnet34, metrics=error_rate)
+learn.fine_tune(1)
+
+uploader = SimpleNamespace(data = ['cat_example.jpg'])
+
+img = PILImage.create(uploader.data[0])
+is_cat,_,probs = learn.predict(img)
+print(f"Is this a cat?: {is_cat}.")
+print(f"Probability it's a cat: {probs[1].item():.6f}")
---
+++ recommender_system_movies_nn.py
@@ -0,0 +1,8 @@
+from fastai.collab import *
+
+path = untar_data(URLs.ML_SAMPLE)
+dls = CollabDataLoaders.from_csv(path/'ratings.csv')
+learn = collab_learner(dls, y_range=(0.5,5.5))
+learn.fine_tune(10)
+
+learn.show_results()
